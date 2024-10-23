2a723b2d1a80a84ac3ac1b43b380e7a816d6827d
suresh-sharma <suresh010480@gmail.com>
2024-10-23 09:26:06 +0530

    update checking

---
+++ data.js
@@ -0,0 +1,304 @@
+export const bio = [
+  "Hello! I'm Suresh Sharma, an experienced Full Stack Software Engineer with a proven ability to upgrade applications, optimize performance, and deliver innovative solutions.",
+  "I am passionate about using my technical skills to drive impactful contributions in challenging projects.",
+  "Thanks for visiting!",
+];
+
+export const skills = [
+  {
+    title: "Languages",
+    skillName: "C#, dotnet core, JavaScript, TypeScript, React js",
+    color: "1",
+    percentage: "80",
+  },
+  {
+    title: "Frameworks/Libraries",
+    skillName: "RxJS, NGRX/NGXS",
+    color: "6",
+    percentage: "70",
+  },
+  {
+    title: "SQL",
+    skillName: "Query, Procedure, Function",
+    color: "4",
+    percentage: "70",
+  },
+  {
+    title: "Version Control",
+    skillName: "GitHub, TFS",
+    color: "7",
+    percentage: "70",
+  },
+  {
+    title: "Tools",
+    skillName: "Debugging, Chrome DevTools, VS",
+    color: "3",
+    percentage: "80",
+  },
+ {
+     title: "Cloud Services",
+     skillName: "Azure App Service, Function, Bus Service, blob Storage, Azure SQL, Docker, Kuberneties,",
+     color: "5",
+     percentage: "50",
+   },
+   {
+    title: "IDE",
+     skillName: "VS Code, Visual Studio 2022",
+     color: "6",
+     percentage: "70",
+   },
+];
+
+export const projects = {
+  webProjects: [
+    {
+      projectName: "Cheapoair.com",
+      image: "images/programmingdiaries.png",
+      summary:
+        "Developed a full stack application to provide content on GDS airline details the internet with B2C interface.",
+      preview: "https://programmingdiaries.herokuapp.com/",
+      techStack: ["Dotnet core", "C#", "Bootstrap", "JavaScript", "Reactjs","SQL"],
+    },
+    {
+      projectName: "Find Your Bank",
+      image: "images/findyourbank.png",
+      summary:
+        "Developed a React application to render a list of banks fetched from API. Filtered the banks based on queries from localstorage, marked favorites banks.",
+      preview: "https://clever-fermi-0d5d76.netlify.app",
+      techStack: ["Reactjs", "Bootstrap", "JavaScript", "Netlify"],
+    },
+    {
+      projectName: "Web Portfolio",
+      image: "images/portfolio.png",
+      summary:
+        "Web Portfolio to showcase acadmics, skills, projects and contact details in better manner.",
+      preview: "https://github.com/suresh-sharma/Travographer-Portal",
+      techStack: ["HTML", "Bootstrap", "JavaScript"],
+    },
+    {
+      projectName: "Resume Builder",
+      image: "images/resume-builder.png",
+      summary:
+        "Browser based editor to build and download Resumes in a customizable templates.",
+      preview: "https://suresh-sharma.github.io/Resume-Builder",
+      techStack: ["HTML", "Bootstrap", "JavaScript"],
+    },
+  ],
+  softwareProjects: [
+    {
+      projectName: "Pizza Ordering ChatBot",
+      image: "images/pizzaorderchatbot.png",
+      summary:
+        "ChatBot using Dialogflow, Firebase database which stores the chat data in the realtime database.",
+      preview: "https://github.com/suresh-sharma/Pizza-Ordering-ChatBot",
+      techStack: ["Dailogflow", "Firebase"],
+    },
+    {
+      projectName: "WhatsApp-Bot",
+      image: "images/whatsappbot.jpg",
+      summary:
+        "Python script which helps to send messages to WhatsApp contacts automatically using selenium and web automation.",
+      preview: "https://github.com/suresh-sharma/WhatsApp-Bot",
+      techStack: ["Selenium", "Chrome Webdriver", "Python"],
+    },
+    {
+      projectName: "Bill Generator",
+      image: "images/billgenerator.png",
+      summary:
+        "GUI to transfer data to excel sheets and generate bills on the local shops.",
+      preview: "https://github.com/suresh-sharma/Bill-Generator",
+      techStack: ["Tkinter", "Openxlpy", "Python"],
+    },
+  ],
+  androidProjects: [
+    {
+      projectName: "NITW-CSE",
+      image: "images/nitwcse.jpg",
+      summary:
+        "The Application display details of Department courses, reference books, research, publication and faculty profile.",
+      preview: "https://github.com/suresh-sharma/NITW-CSE",
+      techStack: ["JAVA", "XML", "Android"],
+    },
+    {
+      projectName: "CareerHigh-App",
+      image: "images/carrerhigh.png",
+      summary:
+        "The Application display the webpages of website careerhigh.in in android devices.",
+      preview: "https://github.com/suresh-sharma/CareerHigh-Android",
+      techStack: ["JAVA", "XML", "Android"],
+    },
+  ],
+  freelanceProjects: [
+    {
+      projectName: "SnylloAir.com",
+      image: "images/snylloair.png",
+      summary:
+        "Developed a company website to showcase the purpose, services and products provided by the company to audience.",
+      preview: "https://www.snylloair.com/",
+      techStack: ["Bootstrap", "JavaScript", "AWS-S3"],
+    },
+    {
+      projectName: "Delivery+",
+      image: "images/AM-Logo-.png",
+      summary: "Android Application to display website in android devices.",
+      preview:
+        "https://play.google.com/store/apps/details?id=com.americanmarket.americanmarketandroid",
+      techStack: ["Android", "JAVA", "Play Store"],
+    },
+  ],
+};
+
+export const experience = [
+  {
+    title: "Birdres Technologies Pvt. Ltd.",
+    duration: "May 2024 - Present",
+    subtitle: "Project Manager",
+    details: [`Over 14+ years of experience in software development using Microsoft.NET technology including analysis/design, programming and architecture.`,
+              `Strong experience of design patterns like Singleton, Factory patterns, etc.`,
+              `Extensive domain knowledge in travel-based project and web portals.`,
+              `Experience in a team leadership role by leading a medium-sized team.`,
+              `7 years of experience using ASP.NET MVC, Entity Framework with LINQ, ASP.NET Web API, and Agile/Scrum Development in various projects.`,
+             `Good experience in writing SQL server programming queries, stored procedures, functions, views, and triggers.`,
+             `Excellent analysing capability to make necessary changes to optimize the performance of code.`,
+              `Cohesive team player & good working experiences both independently and in a team-oriented, collaborative environment.`,
+              `Strong experience to develop and deliver technical solutions to business requirements and assignments according to schedules & deadlines.`,
+              `Excellent ability to develop appropriate documentation for architectural, design, technical, implementation, and test activities.`
+             ],
+    tags: ["JavaScript", "Reactjs", "C#","dotnet core API", "Nodejs", "Docker", "Kuberneties"],
+    icon: "truck ",
+  },
+  {
+    title: "Easyrewardz Software Services",
+    duration: "December 2023 - February 2024",
+    subtitle: "Senior Technical Lead",
+    details: [`Over 14+ years of experience in software development using Microsoft.NET technology including analysis/design, programming and architecture.`,
+              `Strong experience of design patterns like Singleton, Adoptor, Pub and Sub.`,
+              `Extensive domain knowledge in Loyalty based project and web portals.`,
+              `Experience in a team leadership role by leading a medium-sized team.`
+    ],
+    tags: [
+      "Asp.net Core",
+      "Mango DB",
+      "MSMQ",
+      "C#",
+      "MySQL",
+    ],
+    icon: "heartbeat",
+  },
+  {
+    title: "Novopay Solutions Private Limited",
+    duration: "June 2020 - April 2022",
+    subtitle: "Software Engineer",
+    details: [
+      `Implemented AePS Services, including Bio-metric eKYC, Cash Withdrawal, Balance Enquiry, Mini-Statements,
+            and Money transfer.`,
+      `Implemented QR Code integration for retailer wallet money loading, Clevertap events for user action tracking,
+            Freshdesk ticketing, Chatbot services, and Retailer onboarding/admin interfaces.`,
+      `Developed retailer QR code system for seamless money loading into wallets, resulting in improved user transactions.`,
+      `Achieved 70 percent code coverage of the entire application by writing unit tests for legacy code.`,
+    ],
+    tags: ["JavaScript", "Angular", "Bootstrap", "Nodejs", "Jenkins"],
+    icon: "qrcode",
+  },
+  {
+    title: "ThinkPedia LLP",
+    duration: "May 2019 - June 2019",
+    subtitle: "SDE Intern",
+    details: [
+      `Developed a customer web application for social media management, supporting the advertisement domain.`,
+    ],
+    tags: ["JavaScript", "Angular", "Bootstrap", "Java", "Spring Boot"],
+    icon: "group",
+  },
+];
+
+export const education = [
+  {
+    title: "B.Tech. in Computer Science and Engineering",
+    duration: "2016 - 2020",
+    subtitle: "National Institute of Technology, Warangal",
+    details: [],
+    tags: [
+      "Data Structures & Algorithms",
+      "Operating Systems",
+      "Database Management System",
+      "Computer Networks",
+      "Compiler Designing",
+      "Cloud Computing",
+    ],
+    icon: "graduation-cap",
+  },
+  {
+    title: "Class 12th in Science and Mathematics",
+    duration: "",
+    subtitle: "Board of Secondary Education, Rajasthan",
+    details: [],
+    tags: ["Physics", "Chemistry", "Mathematics"],
+    icon: "book",
+  },
+];
+
+export const footer = [
+//   {
+//     label: "Dev Profiles",
+//     data: [
+//       {
+//         text: "Stackoverflow",
+//         link: "https://stackoverflow.com/users/8461233/vinay-somawat",
+//       },
+//       {
+//         text: "GitHub",
+//         link: "https://github.com/suresh-sharma",
+//       },
+//       {
+//         text: "LeetCode",
+//         link: "https://leetcode.com/somawatvinay/",
+//       },
+//     ],
+//   },
+//   {
+//     label: "Resources",
+//     data: [
+//       {
+//         text: "Enable Dark/Light Mode",
+//         func: "enableDarkMode()",
+//       },
+//       {
+//         text: "Print this page",
+//         func: "window.print()",
+//       },
+//       {
+//         text: "Clone this page",
+//         link: "https://github.com/suresh-sharma/blog",
+//       },
+//     ],
+//   },
+  {
+    label: "Links",
+    data: [
+       {
+         text: "Linkedin",
+         link: "https://www.linkedin.com/in/suresh-sharma/",
+       },
+       {
+         text: "Twitter",
+         link: "https://twitter.com/dreamcoding",
+       },
+      {
+        text:"StackOverflow",
+        link : "https://stackoverflow.com/users/5726160/suresh-sharma"
+      },
+       {
+        text: "suresh-sharma/PortFolio",
+        link: "https://github.com/suresh-sharma/blog",
+      },
+    ],
+  },
+  {
+    label: "copyright-text",
+    data: [
+      "Made with &hearts; by Suresh Sharma.",
+      "&copy; No Copyrights. Feel free to use this template.",
+    ],
+  },
+];
